991d8104f99be9917d5c36c1d00196eb65977a59
Dirk Porsche <dirkporsche78@googlemail.com>
2023-12-02 22:14:13 +0100

    fix: fewo switching

--- src/routes/(pages)/accomodations/[accoHandle]/+page.svelte
+++ src/routes/(pages)/accomodations/[accoHandle]/+page.svelte
@@ -6,7 +6,7 @@
   import { onMount } from 'svelte';
   
   export let data: App.PageData & Record<string, any>;
-  let acco = accos.find( (a) => a.path == $page.url.pathname )
+  $: acco = accos.find( (a) => a.path == $page.url.pathname )
   
   onMount( () => {
     if( !acco ) {
@@ -23,17 +23,18 @@
 
 
 {#if acco}
-  {#if acco.displayName }
-  
-  <h1>{acco.displayName}</h1>
-  {/if}
-  {#if acco.siteContent}
-    {#each acco.siteContent as s}
-    <div class="section-wrapper">
-    <Section {...s}/>
-    </div>
-    {/each}
-  {/if}
+  {#key acco.displayName}
+    {#if acco.displayName }  
+    <h1>{acco.displayName}</h1>
+    {/if}
+    {#if acco.siteContent}
+      {#each acco.siteContent as s}
+      <div class="section-wrapper">
+      <Section {...s}/>
+      </div>
+      {/each}
+    {/if}
+  {/key}
 {/if}
 
 
